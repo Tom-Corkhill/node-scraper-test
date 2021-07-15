26c86b469a873b82d8421bdd61a99882582be60f
Tom-Corkhill <63905667+Tom-Corkhill@users.noreply.github.com>
2021-07-15 15:46:32 +0100

    add email parser function

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const knwl = require('knwl.js');
 const request = require('request');
 
 
+// Email Addresses to be tested
+const testEmails = [
+    'tim@canddi.com/',
+    'johnsmith@showsec.co.uk',
+    'janedoe@onvi.com',
+    'alexsmith@vauxhall.co.uk'
+];
+
+function parseEmails(email) {
+    // Split the email and get website to be scraped
+    let split = email.split("@")[1];
+
+    // Merge website
+    let website = "https://www." + split;
+    return website;
+}
+
+
 request('https://www.canddi.com/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
         console.log(html);
     }
-})
+});
