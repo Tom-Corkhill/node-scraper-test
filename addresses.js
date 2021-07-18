bd4519ca653fded32c4c420644a9ab29a5a12895
Tom-Corkhill <63905667+Tom-Corkhill@users.noreply.github.com>
2021-07-18 09:45:56 +0100

    create address file

---
+++ addresses.js
@@ -0,0 +1,13 @@
+function addresses(knwl) {
+    
+    this.languages = {
+        'english': true,
+    };
+    
+    this.calls = function() {
+
+        return results;
+    };
+};
+
+module.exports = addresses;
