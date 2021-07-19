545d225345fed98c59ce79afc8fd07d5af518607
Tom-Corkhill <63905667+Tom-Corkhill@users.noreply.github.com>
2021-07-19 10:12:27 +0100

    create function to find addresses from a postcode

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const knwl = require('knwl.js');
 
 const knwlInstance = new knwl('english');
 knwlInstance.register('internationalPhones', require('./internationalPhones'));
+knwlInstance.register('postCode', require('./postCode'));
 
 const strInputEmail = 'tim@canddi.com';
 const objFinalOutput = {
@@ -11,6 +12,20 @@ const objFinalOutput = {
     arrAddresses: [],
     arrPhones: []
 }
+
+request(getDomain(strInputEmail), (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        
+        const $ = cheerio.load(html);
+        knwlInstance.init($.html());
+
+        getEmails();
+        getAddresses();
+        getPhones();
+
+        delayLog();
+    }
+});
 
 function getDomain(strInputEmail) {
     let strDomain = "https://www." + strInputEmail.split("@")[1];
@@ -21,7 +36,7 @@ function getEmails() {
     let arrFoundEmails = knwlInstance.get('emails');
     if (arrFoundEmails) {
         for (let i = 0; i < arrFoundEmails.length; i++) {
-            if (!objFinalOutput.arrEmails.includes(arrFoundEmails[i].address)) {
+            if ((!objFinalOutput.arrEmails.includes(arrFoundEmails[i].address)) && (arrFoundEmails[i].address !== strInputEmail)) {
                 objFinalOutput.arrEmails.push(arrFoundEmails[i].address);
             }
         }
@@ -29,10 +44,27 @@ function getEmails() {
 }
 
 function getAddresses() {
-    let arrFoundAddresses = knwlInstance.get('places');
-    console.log(arrFoundAddresses);
-}
+    let arrFoundAddresses = knwlInstance.get('postCode');
 
+    if (arrFoundAddresses) {
+        for (let i = 0; i < arrFoundAddresses.length; i++) {
+            let strPostCode = arrFoundAddresses[i].postCode;
+            let strPostCodeDomain = "https://checkmypostcode.uk/" + strPostCode;
+
+            // Enters postcode in "checkmypostcode.uk" and returns address
+            request(strPostCodeDomain, (error, response, html) => {
+                if (!error && response.statusCode === 200) {
+                    const $ = cheerio.load(html);
+                    let strAddress = ($('.panel').text().replace(/\s\s+/g, '').match(/.+?(?=[a-zA-z]{1,2}\d{1,2})/)[0]);
+                    let strFinalAddress = strAddress + strPostCode;
+                    if(!objFinalOutput.arrAddresses.includes(strFinalAddress)) {
+                        objFinalOutput.arrAddresses.push(strFinalAddress);
+                    }
+                }
+            });
+        }
+    }
+}
 
 function getPhones() {
     let arrFoundPhones = knwlInstance.get('internationalPhones');
@@ -45,18 +77,8 @@ function getPhones() {
     }
 }
 
-
-request(getDomain(strInputEmail), (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-        
-        const $ = cheerio.load(html);
-        const strHTML = $.html();
-        knwlInstance.init($.html());
-
-        getEmails();
-        // getAddresses();
-        getPhones();
-
-        console.log(objFinalOutput);
-    }
-});
+const delayLog = async () => {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    await delay(1500);
+    console.log(objFinalOutput);
+  };
